15ed64e81a0232ea6624b4bc620550fe483b4074
stella180303 <stelladdvnx1803@gmail.com>
2025-06-15 22:27:26 +0700

    edit

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
-$app = require __DIR__.'/bootstrap/app.php';
+$app = require __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $request = Illuminate\Http\Request::capture();
 $response = $kernel->handle($request);
