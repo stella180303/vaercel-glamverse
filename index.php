c0e3cb2450d9e7a7221584d6c751bd55f75f5575
stella180303 <stelladdvnx1803@gmail.com>
2025-06-15 22:21:46 +0700

    edit

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$request = Illuminate\Http\Request::capture();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
