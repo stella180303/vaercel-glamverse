d24ce56f557f2dfe251981b5e574b872c3e89660
stella180303 <stelladdvnx1803@gmail.com>
2025-06-15 22:40:47 +0700

    edit

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
-require __DIR__ . '/../public/index.php';
+// Debug
+if (!file_exists(__DIR__.'/../public/index.php')) {
+    die('Tidak ditemukan public/index.php di ' . realpath(__DIR__.'/../public/index.php'));
+}
+
+require __DIR__.'/../public/index.php';
